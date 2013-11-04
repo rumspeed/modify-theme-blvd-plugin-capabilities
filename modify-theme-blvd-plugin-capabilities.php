59017b3d2caa3060d3433f513b2f3a36d4e9661d
Scot Rumery <scot@rumspeed.com>
2013-11-04 06:18:44 -0500

    add GPLv2

--- modify-theme-blvd-plugin-capabilities.php
+++ modify-theme-blvd-plugin-capabilities.php
@@ -5,7 +5,26 @@ Description: Modify capabilities to allow Editors access to Theme Blvd plugins
 Version: 0.3.1
 Author: Scot Rumery
 Author URI: http://rumspeed.com/scot-rumery/
+License: GPLv2
 */
+
+/*  Copyright 2013  Scot Rumery (email : scot@rumspeed.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 
 
 
